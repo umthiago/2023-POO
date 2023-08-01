4fceca320fed64d98d62366099133e4939297d18
Thiago <124899004+umthiago@users.noreply.github.com>
2023-08-01 08:47:09 -0300

    Create Cachorro.java

---
+++ Atividades 3º Bim/Atv 01/notebook/Cachorro.java
@@ -0,0 +1,24 @@
+public class Cachorros extends Mamiferos{
+    private String raca; //atributo raca
+
+    public Cachorros(String raca2, String nome, int idade){
+        super(nome, idade);
+        this.raca = raca2;
+    }
+
+    public String getRaca(){
+        return this.raca;
+    }
+
+
+
+    public void setRaca(String raca2){
+        this.raca = raca2;
+    }
+
+
+
+    public void abanarRabo(){
+        System.out.println("O cachorro está abanando o rabo.");
+    }
+}
